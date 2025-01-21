67b85fd07e36aedafa0d4107ada3d5cb1d53ee34
EHEN123 <dlgpdms6424@gmail.com>
2025-01-22 00:59:25 +0900

    [0.1.2]모바일 click sound js 조정

--- src/js/common.js
+++ src/js/common.js
@@ -1,23 +1,31 @@
 console.log('뭐 해?');
 
 // 클릭 사운드
-const clickSound = new Audio('/asset/sound/click_sound.mp3');
-clickSound.volume = 0.4;
+let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+let buffer;
+
+fetch('/asset/sound/click_sound.mp3')
+  .then(response => response.arrayBuffer())
+  .then(data => audioContext.decodeAudioData(data))
+  .then(decodedData => {
+    buffer = decodedData;
+  });
 
 document.addEventListener('click', () => {
-  if (!clickSound.paused) return;
-  clickSound.currentTime = 0;
-  clickSound.play();
+  if (!buffer) return;
+  let source = audioContext.createBufferSource();
+  source.buffer = buffer;
+  source.connect(audioContext.destination);
+  source.start(0);
 });
+
 
 // 100vh 모바일 ui(주소창, 하단바 등)에따라 유동적으로
 const updateViewportHeight = () => {
-  const vh = window.innerHeight * 0.01; // 1vh에 해당하는 값 계산
+  const vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 };
 
-// 초기 설정
 updateViewportHeight();
 
-// 화면 크기 변경 시 업데이트
 window.addEventListener("resize", updateViewportHeight);
