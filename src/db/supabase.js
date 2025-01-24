bac504f4eb1e99f945180017c787fdd607801d6b
EHEN123 <dlgpdms6424@gmail.com>
2025-01-24 21:05:55 +0900

    [1.5.3]재확인

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
@@ -6,9 +6,9 @@ const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 console.log("Supabase URL:", process.env.REACT_APP_SUPABASE_URL);
 console.log("Supabase Key:", process.env.REACT_APP_SUPABASE_KEY);
 
+
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Supabase URL and API Key must be provided.");
 }
-
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
