1f4302814c1d389b22274437cdc31361bbb2ecf1
EHEN123 <dlgpdms6424@gmail.com>
2025-07-01 02:43:46 +0900

    [2.9.4]스토리아이템 수정, 랜덤박스 확률 조정

--- src/db/randomBoxItems.js
+++ src/db/randomBoxItems.js
@@ -1,15 +1,16 @@
 const randomBoxItems = [
-  { id: 1, name: "포츈쿠키", chance: 20 },
-  { id: 2, name: "버려진 쪽지", chance: 20 },
-  { id: 3, name: "핫바", chance: 20 },
-  { id: 6, name: "망신부적", chance: 5 },
-  { id: 5, name: "손재부적", chance: 5 },
-  { id: 7, name: "박복부적", chance: 5 },
-  { id: 8, name: "따봉고슴도치 스티커", chance: 5 },
-  { id: 11, name: "뱃지", chance: 2 },
-  { id: 9, name: "확성기", chance: 5 },
-  { id: 10, name: "페인트통", chance: 5 },
-  { id: 13, name: "빨간 펜", chance: 2 }, // 특수 아이템
-];
+  { id: 1, name: "포츈쿠키", chance: 23 },
+  { id: 2, name: "버려진 쪽지", chance: 23 },
+  { id: 3, name: "핫바", chance: 23 },
 
-export default randomBoxItems;
+  { id: 6, name: "망신부적", chance: 3 },
+  { id: 5, name: "손재부적", chance: 3 },
+  { id: 7, name: "박복부적", chance: 3 },
+  { id: 8, name: "따봉고슴도치 스티커", chance: 3 },
+  { id: 9, name: "확성기", chance: 3 },
+  { id: 10, name: "페인트통", chance: 3 },
+
+  { id: 11, name: "뱃지", chance: 1.5 },
+  
+  { id: 13, name: "빨간 펜", chance: 1.5 },
+];
